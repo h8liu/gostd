9ab611fe11f764bf29bb0df9a2d7dacd22b7629b
He Liu <liulonnie@gmail.com>
2015-01-18 19:51:11 -0800

    remove cmd

--- main.go
+++ main.go
@@ -1,3 +1,34 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"go/build"
+	"strings"
+
+	"golang.org/x/tools/go/buildutil"
+)
+
+func packages() []string {
+	c := &build.Default
+	hold := c.GOPATH
+	c.GOPATH = ""
+	pkgs := buildutil.AllPackages(c)
+	c.GOPATH = hold
+
+	var ret []string
+	for _, p := range pkgs {
+		if strings.HasPrefix(p, "cmd/") || p == "cmd" {
+			continue
+		}
+
+		ret = append(ret, p)
+	}
+	return ret
+}
+
+func main() {
+	pkgs := packages()
+	for _, p := range pkgs {
+		fmt.Println(p)
+	}
+}

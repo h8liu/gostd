05bf9e18ec4386e8a8b871f47c3130e10e7c6381
He Lonnie Liu <liulonnie@gmail.com>
2015-01-20 13:43:19 -0800

    adding the results

--- dat.go
+++ dat.go
@@ -62,11 +62,68 @@ func runeHtml(ch rune) string {
 	return html.EscapeString(string(ch))
 }
 
-func tokClass(t token.Token) string {
+var builtinTypes = []string{
+	"int",
+	"uint",
+	"int32",
+	"uint32",
+	"int16",
+	"uint16",
+	"int8",
+	"uint8",
+	"byte",
+	"rune",
+	"error",
+	"string",
+	"float32",
+	"float64",
+	"complex64",
+	"complex128",
+	"uintptr",
+	"bool",
+	"map",
+}
+
+var builtinFuncs = []string{
+	"len",
+	"cap",
+	"close",
+	"complex",
+	"delete",
+	"imag",
+	"panic",
+	"print",
+	"println",
+	"real",
+	"recover",
+	"make",
+	"append",
+	"new",
+	"copy",
+}
+
+var builtinFuncMap = makeMap(builtinFuncs)
+var builtinTypeMap = makeMap(builtinTypes)
+
+func makeMap(lst []string) map[string]struct{} {
+	ret := make(map[string]struct{})
+	for _, s := range lst {
+		ret[s] = struct{}{}
+	}
+
+	return ret
+}
+
+func tokClass(t token.Token, lit string) string {
 	switch {
 	case t == token.COMMENT:
 		return "comment"
 	case t == token.IDENT:
+		if _, found := builtinFuncMap[lit]; found {
+			return "builtinfunc"
+		} else if _, found := builtinTypeMap[lit]; found {
+			return "builtintype"
+		}
 		return "ident"
 	case t >= token.INT && t <= token.IMAG:
 		return "num"
@@ -79,6 +136,13 @@ func tokClass(t token.Token) string {
 	}
 
 	return "na"
+}
+
+func lineHeader(line int) string {
+	if line == 1 || line % 5 == 0 {
+		return fmt.Sprintf(`<span class="lineno">%d</span>`, line)
+	}
+	return fmt.Sprintf(`<span class="lineno"></span>`)
 }
 
 func (f *file) html() []byte {
@@ -94,7 +158,9 @@ func (f *file) html() []byte {
 
 	fmt.Fprint(out, htmlHeader)
 
-	fmt.Fprint(out, `<div class="code">\n`)
+	fmt.Fprint(out, `<div class="code">`+"\n")
+	line := 1
+	fmt.Fprint(out, lineHeader(line))
 
 	for _, t := range f.toks {
 		if t.tok == token.EOF {
@@ -120,6 +186,10 @@ func (f *file) html() []byte {
 			off += n
 
 			fmt.Fprint(out, runeHtml(ch))
+			if ch == '\n' {
+				line++
+				fmt.Fprint(out, lineHeader(line))
+			}
 		}
 
 		if off != toff {
@@ -127,8 +197,12 @@ func (f *file) html() []byte {
 			panic("rune not aligned")
 		}
 
+		lit := t.lit
+		if t.tok.IsOperator() {
+			lit = t.tok.String()
+		}
 
-		nb := len([]byte(t.lit))
+		nb := len([]byte(lit))
 
 		buf := make([]byte, nb)
 		_, e := r.Read(buf)
@@ -138,21 +212,25 @@ func (f *file) html() []byte {
 
 		str := string(buf)
 
-		if t.lit != str {
-			log.Printf("t=%s lit=%q str=%q", t.tok.String(), t.lit, str)
+		if lit != str {
+			log.Printf("t=%s lit=%q str=%q", t.tok.String(), lit, str)
 			panic("lit unmatch")
 		}
 
-		fmt.Fprintf(out, `<span class="%s">`, tokClass(t.tok))
+		fmt.Fprintf(out, `<span class="%s">`, tokClass(t.tok, lit))
 		for _, ch := range str {
 			fmt.Fprint(out, runeHtml(ch))
+			if ch == '\n' {
+				line++
+				fmt.Fprint(out, lineHeader(line))
+			}
 		}
 		fmt.Fprintf(out, `</span>`)
 
 		off += nb
 	}
 
-	fmt.Fprint(out, `\n</div>\n`)
+	fmt.Fprint(out, "\n</div>\n")
 
 	fmt.Fprint(out, htmlFooter)
 
--- header.go
+++ header.go
@@ -6,21 +6,7 @@ const htmlHeader = `<!doctype html>
 <head>
 	<meta charset="utf-8" />
 	<title>Go standard library</title>
-	<style type="text/css">
-		div.code {
-			font-family: "Consolas", monospace;
-			color: #333;
-			font-size: 13px;
-		}
-
-		.comment { color: #999; }
-		.ident { color: #009; }
-		.num { color: #090; }
-		.string { color: #909; }
-		.op { color: #903; }
-		.keyword { color: #339; }
-		.na { color: #f00; }
-	</style>
+	<link rel="stylesheet" type="text/css" href="/style.css">
 </head>
 
 <body>
